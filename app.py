c04da5f0ab0feccf9132a15670e40c1217e0683d
Codes-of-SK <codesofsk@gmail.com>
2024-07-17 19:22:30 +0530

    edited commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+nltk.download("punkt")
 ps = PorterStemmer()
 
 
@@ -21,7 +22,7 @@ def transform_text(text):
     y.clear()
 
     for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
+        if i not in stopwords.words("english") and i not in string.punctuation:
             y.append(i)
 
     text = y[:]
@@ -32,14 +33,15 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+
+tfidf = pickle.load(open("vectorizer.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
-if st.button('Predict'):
+if st.button("Predict"):
 
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
