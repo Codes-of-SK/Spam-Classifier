b4ff16076c0860935b75b1d0cad6f0b2c95af5d0
Codes-of-SK <codesofsk@gmail.com>
2024-07-17 18:44:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pickle
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+import string
+
+ps = PorterStemmer()
+tfidf = pickle.load(open("vectorizer.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
+
+st.title("Email/SMS Classifier")
+
+
+def transform_text(text):
+    # to lower cases
+    text = text.lower()
+
+    # tokenize
+    text = nltk.word_tokenize(text)
+
+    # remove special chars
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    # remove stop words(words that does not provide meaning) and punctuation
+    for i in text:
+        if i not in stopwords.words("english") and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    # Stemming
+    for i in text:
+        y.append(ps.stem(i))
+    text = y[:]
+    y.clear()
+    return " ".join(text)
+
+
+input_sms = st.text_area("Enter the message")
+if st.button("Predict"):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
+st.text("Prediction may or may not be accurate. Do not use for institutional purposes.")
+
+
+footer = """<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-color: white;
+    color: white;
+    text-align: center;
+}
+</style>
+<div class="footer">
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 24px; height: 24px;">
+        <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)"/>
+        <path d="m12 1a10.89 10.89 0 0 0 -11 10.77 10.79 10.79 0 0 0 7.52 10.23c.55.1.75-.23.75-.52s0-.93 0-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 0 0 -1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 0 1 1.68 1.11 2.37 2.37 0 0 0 3.2.89 2.33 2.33 0 0 1 .7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 0 1 1.11-2.91 3.78 3.78 0 0 1 .11-2.84s.93-.29 3 1.1a10.68 10.68 0 0 1 5.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 0 1 .11 2.84 4.15 4.15 0 0 1 1.17 2.89c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 0 1 .75 2v2.95s.2.63.75.52a10.8 10.8 0 0 0 7.5-10.22 10.89 10.89 0 0 0 -11-10.77" fill="white"/>
+    </svg>
+    <p>Learnt and developed by <a style='display: block; text-align: center;' href="https://github.com/Codes-of-SK" target="_blank">CODES-OF-SK</a></p>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
